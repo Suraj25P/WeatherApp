db420a3695523c9c2653067cb0db2ce353c901c3
Suraj25P <surajrprabhu99@gmail.com>
2019-06-06 21:22:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "3585775f387b0d0cba6c5b3dc41b8167";
+const API_KEY = "";
 
 class App extends React.Component {
   state = {
